ada9349034e3ced4555a6012260b858ad9fd27d8
euzu <euzu@proton.me>
2025-06-18 09:15:23 +0200

    Allow multiple strm output for diffrent export styles

--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -7,7 +7,7 @@ pub mod playlist_repository;
 pub mod m3u_repository;
 pub mod xtream_repository;
 pub mod epg_repository;
-pub mod kodi_repository;
+pub mod strm_repository;
 pub mod m3u_playlist_iterator;
 pub mod xtream_playlist_iterator;
 pub mod user_repository;
