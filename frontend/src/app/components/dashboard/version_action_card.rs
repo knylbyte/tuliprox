6be54869c6f9e7ff8773825a79f37f913a58f541
euzu <euzu@proton.me>
2025-08-12 17:35:34 +0200

    fixed logo url with path prefix

--- frontend/src/app/components/dashboard/version_action_card.rs
+++ frontend/src/app/components/dashboard/version_action_card.rs
@@ -1,7 +1,11 @@
 use gloo_utils::window;
 use yew::prelude::*;
 use yew_i18n::use_translation;
+use shared::utils::concat_path_leading_slash;
 use crate::app::components::{ActionCard, TextButton};
+use crate::app::ConfigContext;
+use crate::hooks::use_service_context;
+use crate::provider::_ServiceContextProps::config;
 
 #[derive(Properties, Clone, PartialEq, Debug)]
 pub struct VersionActionProps {
@@ -12,6 +16,7 @@ pub struct VersionActionProps {
 #[function_component]
 pub fn VersionActionCard(props: &VersionActionProps) -> Html {
     let translate = use_translation();
+    let services = use_service_context();
 
     let handle_url = {
         Callback::from(move |_| {
@@ -22,9 +27,17 @@ pub fn VersionActionCard(props: &VersionActionProps) -> Html {
         })
     };
 
+    let logo_url = {
+        if let Some(web_path) = services.config.ui_config.web_path.as_ref() {
+            concat_path_leading_slash(web_path,  "/assets/tuliprox-logo.svg")
+
+        } else {
+            "/assets/tuliprox-logo.svg".to_owned()
+        }
+    };
 
     html! {
-        <ActionCard icon="/assets/tuliprox-logo.svg" title={props.version.clone()}
+        <ActionCard icon={logo_url} title={props.version.clone()}
         subtitle={props.build_time.clone()}>
           <TextButton name="realeases" title={translate.t("LABEL.RELEASES")} icon="Link" onclick={handle_url} />
         </ActionCard>
