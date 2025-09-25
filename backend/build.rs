fccd832f5742eafacbb900e586453e309e876904
knylbyte <philippmielke@icloud.com>
2025-09-25 15:22:21 +0200

    del: test

--- backend/build.rs
+++ backend/build.rs
@@ -1,7 +1,6 @@
 use vergen::{BuildBuilder, Emitter};
 
 fn main() {
-    println!("test run for gh workflow");
     if let Ok(build) = BuildBuilder::all_build() {
         if let Ok(emitter) = Emitter::default().add_instructions(&build) {
             let _ = emitter.emit();
