b2df3b3620f9a1ddca51b789a018e829154ac3df
euzu <euzu@proton.me>
2025-11-06 15:15:52 +0100

    Fixed custom share response

--- backend/src/api/model/streams/shared_stream_manager.rs
+++ backend/src/api/model/streams/shared_stream_manager.rs
@@ -350,7 +350,9 @@ impl SharedStreamManager {
             };
 
             if is_empty {
-                self.unregister(stream_url.as_ref().unwrap(), send_stop_signal).await;
+                if let Some(url) = stream_url.as_ref() {
+                    self.unregister(url, send_stop_signal).await;
+                }
             }
 
             if let Some(client_stop_signal) = tx {
