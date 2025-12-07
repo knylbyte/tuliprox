f342121d9e6daac319e4bcebca84fc420c36583b
knylbyte <40831653+knylbyte@users.noreply.github.com>
2025-12-07 21:53:19 +0100

    feat: check log level für client request logging

--- backend/src/api/main_api.rs
+++ backend/src/api/main_api.rs
@@ -393,9 +393,9 @@ fn add_rate_limiter(
 
 async fn log_req(req: Request, next: Next) -> impl axum::response::IntoResponse {
     if !log::log_enabled!(log::Level::Debug) {
-        return
+        return next.run(req).await;
     }
-    
+
     let method = req.method().clone();
     let uri = req.uri().clone();
 
